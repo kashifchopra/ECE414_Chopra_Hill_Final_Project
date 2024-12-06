0b139fb930b9db2abd4721fcc25853608e305f2a
Benjamin Hill <ben@Benjamins-MacBook-Pro-2.local>
2024-12-06 15:03:57 -0500

    PIR jawn

---
+++ Arduino/MotionSensor/PIR_sense.cpp
@@ -0,0 +1,89 @@
+#include <SPI.h>
+#include <Arduino.h>
+#include "PIR_sense.h"
+//#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+#include <stdio.h>
+#include <stdbool.h>
+//#include "lora_trans.h" -- when made
+
+//Define PICO pin connections
+
+#define SS_PIN 16  // GPIO 17 for PIR
+
+//Define states global var
+PIR_STATES STATE = INIT; //initialize to init
+
+bool motion;                //PIR data, high or low
+bool occupied;              //boolean flag
+uint32_t internal_cnt;      //counter incremented once every second in OCCUPIED state
+uint32_t occ_tmr_cnt;       //cycles of no motion
+uint32_t timer_limit = 20;  //SUBJECT TO CHANGE FOR TESTING PURPOSES
+const uint32_t pir_gpio = 16;
+
+void pir_init() {  //low level code to initialize port
+  adc_init();
+  _gpio_init(pir_gpio);
+  gpio_set_dir(pir_gpio, GPIO_IN);
+  Serial.println("PIR init");
+}
+
+bool pir_read() {  //boolean output of pir
+  motion = gpio_get(pir_gpio);
+  Serial.print("Motion Detected: ");
+  Serial.println(motion);
+  return motion;
+}
+
+void tickFnct_pir() {
+
+  switch (STATE) {
+
+    case INIT:
+      Serial.println(F("Entering Intial State"));
+      if (motion) {
+        STATE = OCC;
+      } else {
+        STATE = UNOCC;
+      }
+      break;
+
+    case UNOCC:
+      Serial.println(F("Unoccupied"));
+      occupied = false;
+      if (motion) {
+        Serial.println("Motion Detected, going to OCC");
+        occupied = true;
+        STATE = OCC;
+      }
+      delay(1000);
+      break;
+
+    case OCC:
+      Serial.println(F("Occupied"));
+      Serial.printf("Minutes Passed: ");
+      Serial.println(F(internal_cnt));
+      Serial.printf("Occ_tmr_cnt: ");
+      Serial.println(F(occ_tmr_cnt));
+      //maybe print internal timer cnt, stuff liek that 
+      
+      internal_cnt++;
+      if (motion) {
+        occ_tmr_cnt = 0;
+      }
+
+      if (internal_cnt >= 60) {  //will occur once every minute
+        if (!motion && occ_tmr_cnt << 20) {
+          occ_tmr_cnt++;
+        }
+      }
+
+      if (occ_tmr_cnt >= 20) {
+        STATE = UNOCC;
+      }
+
+      delay(1000);
+      
+  }
+}
